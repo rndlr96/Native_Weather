11d06fc686a4b5aa2107633bc0dd7eaa7debb387
Pandog <rndlr96@gmail.com>
2018-03-22 02:01:57 +0900

    string in fetch is start and end with ``

--- App.js
+++ App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator, StatusBar } from 'react-native';
-import Weather from "./Weather";
+import Rain from "./Weather/Rain";
+import Sunny from "./Weather/Sunny";
+import Cloud from "./Weather/Cloud";
+
+const API_KEY = '45ae472d107e47842765826a9194ede3';
 
 export default class App extends React.Component {
 
   state = {
-    isLoaded: true
+    isLoaded: false,
+    error: null
   }
 
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude)
+      },
+      error => {
+          this.setState({
+            isLoaded: false,
+            error: "Something went Wrong"
+          });
+      }
+    );
+  }
+
+  _getWeather = (lat, long) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+    });
+  };
+
    render() {
-     const {isLoaded} = this.state;
+     const {isLoaded, error} = this.state;
      return (
        <View style={styles.container}>
          <StatusBar barStyle = "light-content"/>
          {
            isLoaded ? (
-             <Weather/>
+             <Cloud/>
            ) : (
              <View style={styles.loading}>
                <ActivityIndicator/>
                <Text style = {styles.loadingtext}>Now I getting the Weather</Text>
+               {error ? <Text style = {styles.errorText}>{error}</Text> : null}
              </View>
            )
          }
@@ -32,6 +60,10 @@ export default class App extends React.Component {
    container: {
      flex: 1,
     backgroundColor: '#fff'
+  },
+
+  errorText: {
+    color: "red"
   },
 
   loading: {
---
+++ Weather/Sunny.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {LinearGradient} from "expo";
+import { Ionicons } from "@expo/vector-icons"
+
+export default class Sunny extends Component{
+  render(){
+    return(
+      <LinearGradient colors={["red", "#fafc8d", "#faff00"]} style={styles.container}>
+        <View style={ styles.upper }>
+          <Ionicons style = { styles.icon } color = "white" size = {144} name = "md-sunny"/>
+          <Text style = { styles.temp }>35</Text>
+        </View>
+
+        <View style={styles.lower}>
+          <Text style = { styles.title }>Raining Now</Text>
+          <Text style = { styles.subtitle }>Must bring user umbrella</Text>
+        </View>
+      </LinearGradient>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  icon: {
+    paddingTop: 70
+  },
+
+  upper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  temp: {
+    fontSize: 38,
+    marginTop: 10,
+    color: 'white',
+    fontWeight: '400'
+  },
+
+  lower: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent:'flex-end',
+  },
+
+  title: {
+    fontSize: 38,
+    color: 'white',
+    marginBottom: 10,
+    fontWeight: '600'
+  },
+
+  subtitle: {
+    fontSize: 24,
+    color: 'white',
+    marginBottom: 70,
+    fontWeight: '400'
+  }
+});
